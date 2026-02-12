66005d6f025a73138516e3265430b97fa605fb33
shoucandanghehe <wallfjjd@gmail.com>
2026-02-13 03:04:51 +0800

    :bug: 修复命令参数解析中的 Missing 类型收窄

--- nonebot/adapters/discord/commands/params.py
+++ nonebot/adapters/discord/commands/params.py
@@ -12,6 +12,8 @@ from ..api import (
     Snowflake,
     User,
 )
+from ..api.model import ApplicationCommandInteractionDataOption
+from ..api.types import Missing, is_not_unset, is_unset
 from ..event import ApplicationCommandInteractionEvent
 
 T = TypeVar("T")
@@ -53,84 +55,100 @@ class OptionParam(Param):
                     return cls(key=arg.key or param.name, validate=True)
         return None
 
+    @staticmethod
+    def _get_options(
+        options: Missing[list[ApplicationCommandInteractionDataOption]],
+    ) -> Optional[list[ApplicationCommandInteractionDataOption]]:
+        if is_unset(options):
+            return None
+        if (
+            options
+            and options[0].type == ApplicationCommandOptionType.SUB_COMMAND_GROUP
+        ):
+            if is_unset(options[0].options):
+                return None
+            options = options[0].options
+        if options and options[0].type == ApplicationCommandOptionType.SUB_COMMAND:
+            if is_unset(options[0].options):
+                return None
+            options = options[0].options
+        return options
+
     @override
     async def _solve(
         self, event: ApplicationCommandInteractionEvent, **kwargs: Any
     ) -> Any:
-        if event.data.options:
-            options = event.data.options
-            if (
-                options
-                and options[0].type == ApplicationCommandOptionType.SUB_COMMAND_GROUP
-            ):
-                options = options[0].options
-            if options and options[0].type == ApplicationCommandOptionType.SUB_COMMAND:
-                options = options[0].options
-            if options:
-                for option in options:
-                    if option.name == self.key:
-                        if (
-                            option.type
-                            in (
-                                ApplicationCommandOptionType.USER,
-                                ApplicationCommandOptionType.CHANNEL,
-                                ApplicationCommandOptionType.ROLE,
-                                ApplicationCommandOptionType.ATTACHMENT,
-                            )
-                            and event.data.resolved
-                            and (
-                                data := getattr(
-                                    event.data.resolved, type_str_mapping[option.type]
-                                )
-                            )
-                        ):
-                            return data[Snowflake(option.value)]
-                        if (
-                            option.type == ApplicationCommandOptionType.MENTIONABLE
-                            and event.data.resolved
-                            and event.data.resolved.users
-                        ):
-                            sid = Snowflake(option.value)
-                            return (
-                                event.data.resolved.users.get(sid),
-                                (
-                                    event.data.resolved.members.get(sid)
-                                    if event.data.resolved.members
-                                    else None
-                                ),
-                            )
-                        if option.type in (
-                            ApplicationCommandOptionType.INTEGER,
-                            ApplicationCommandOptionType.STRING,
-                            ApplicationCommandOptionType.NUMBER,
-                            ApplicationCommandOptionType.BOOLEAN,
-                        ):
-                            return option.value
+        options = self._get_options(event.data.options)
+        if options is None:
+            return None
+
+        for option in options:
+            if option.name != self.key:
+                continue
+            if option.type in (
+                ApplicationCommandOptionType.USER,
+                ApplicationCommandOptionType.CHANNEL,
+                ApplicationCommandOptionType.ROLE,
+                ApplicationCommandOptionType.ATTACHMENT,
+                ApplicationCommandOptionType.MENTIONABLE,
+            ) and is_not_unset(event.data.resolved):
+                if not isinstance(option.value, (str, int)) or isinstance(
+                    option.value, bool
+                ):
+                    return None
+                if option.type != ApplicationCommandOptionType.MENTIONABLE:
+                    data = getattr(event.data.resolved, type_str_mapping[option.type])
+                    if is_not_unset(data):
+                        return data[Snowflake(option.value)]
+                elif is_not_unset(event.data.resolved.users):
+                    sid = Snowflake(option.value)
+                    members = (
+                        None
+                        if is_unset(event.data.resolved.members)
+                        else event.data.resolved.members
+                    )
+                    return (
+                        event.data.resolved.users.get(sid),
+                        members.get(sid) if members else None,
+                    )
+            if option.type in (
+                ApplicationCommandOptionType.INTEGER,
+                ApplicationCommandOptionType.STRING,
+                ApplicationCommandOptionType.NUMBER,
+                ApplicationCommandOptionType.BOOLEAN,
+            ) and is_not_unset(option.value):
+                return option.value
         return None
 
 
 def get_command_message(
     event: ApplicationCommandInteractionEvent,
 ) -> Union[MessageGet, None]:
+    if event.data.type != ApplicationCommandType.MESSAGE:
+        return None
+
     if (
-        event.data.type == ApplicationCommandType.MESSAGE
-        and event.data.target_id
-        and event.data.resolved
-        and event.data.resolved.messages
+        is_unset(event.data.target_id)
+        or is_unset(event.data.resolved)
+        or is_unset(event.data.resolved.messages)
     ):
-        return event.data.resolved.messages.get(event.data.target_id)
-    return None
+        return None
+
+    return event.data.resolved.messages.get(event.data.target_id)
 
 
 def get_command_user(event: ApplicationCommandInteractionEvent) -> Union[User, None]:
+    if event.data.type != ApplicationCommandType.USER:
+        return None
+
     if (
-        event.data.type == ApplicationCommandType.USER
-        and event.data.target_id
-        and event.data.resolved
-        and event.data.resolved.users
+        is_unset(event.data.target_id)
+        or is_unset(event.data.resolved)
+        or is_unset(event.data.resolved.users)
     ):
-        return event.data.resolved.users.get(event.data.target_id)
-    return None
+        return None
+
+    return event.data.resolved.users.get(event.data.target_id)
 
 
 CommandOption = Annotated[T, CommandOptionType()]
