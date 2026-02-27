269e2b6f34088328765fe0343c1917c6f38cf8cb
shoucandanghehe <wallfjjd@gmail.com>
2026-02-28 03:11:23 +0800

    ♻️ 拆分 api/model：迁移定时事件 RecurrenceRule 模型

--- nonebot/adapters/discord/api/models/guild_scheduled_events.py
+++ nonebot/adapters/discord/api/models/guild_scheduled_events.py
@@ -10,13 +10,70 @@ from ..types import (
     UNSET,
     GuildScheduledEventEntityType,
     GuildScheduledEventPrivacyLevel,
+    GuildScheduledEventRecurrenceRuleFrequency,
+    GuildScheduledEventRecurrenceRuleMonth,
+    GuildScheduledEventRecurrenceRuleWeekday,
     GuildScheduledEventStatus,
     Missing,
     MissingOrNullable,
 )
 
 if TYPE_CHECKING:
-    from ..model import GuildMember, RecurrenceRule, User
+    from ..model import GuildMember, User
+
+
+class RecurrenceRule(BaseModel):
+    """Discord's recurrence rule is a subset of the behaviors defined
+    in the iCalendar RFC and implemented by python's dateutil rrule
+
+    see https://discord.com/developers/docs/resources/guild-scheduled-event#guild-scheduled-event-recurrence-rule-object
+    """
+
+    start: datetime.datetime
+    """Starting time of the recurrence interval"""
+
+    end: datetime.datetime | None = None
+    """Ending time of the recurrence interval"""
+
+    frequency: GuildScheduledEventRecurrenceRuleFrequency
+    """How often the event occurs"""
+
+    interval: int
+    """The spacing between the events, defined by frequency. For example,
+    frecency of WEEKLY and an interval of 2 would be "every-other week"""
+
+    by_weekday: list[GuildScheduledEventRecurrenceRuleWeekday] | None = None
+    """Set of specific days within a week for the event to recur on"""
+
+    by_n_weekday: list[GuildScheduledEventRecurrenceRuleN_WeekdayStructure] | None = (
+        None
+    )
+    """List of specific days within a specific week (1-5) to recur on"""
+
+    by_month: list[GuildScheduledEventRecurrenceRuleMonth] | None = None
+    """Set of specific months to recur on"""
+
+    by_month_day: int | None = None
+    """Set of specific dates within a month to recur on"""
+
+    by_year_day: int | None = None
+    """Set of days within a year to recur on (1-364)"""
+
+    count: int | None = None
+    """The total amount of times that the event is allowed to recur before stopping"""
+
+
+class GuildScheduledEventRecurrenceRuleN_WeekdayStructure(BaseModel):  # noqa: N801
+    """Guild Scheduled Event Recurrence Rule - N_Weekday Structure
+
+    see https://discord.com/developers/docs/resources/guild-scheduled-event#guild-scheduled-event-recurrence-rule-object-guild-scheduled-event-recurrence-rule-nweekday-structure
+    """
+
+    n: int
+    """The week to reoccur on. 1 - 5"""
+
+    day: GuildScheduledEventRecurrenceRuleWeekday
+    """The day within the week to reoccur on"""
 
 
 class GuildScheduledEvent(BaseModel):
