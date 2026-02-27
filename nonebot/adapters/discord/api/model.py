7cfab412c82c28c45180dcaf3ffa0a9bea79634c
shoucandanghehe <wallfjjd@gmail.com>
2026-02-28 02:16:20 +0800

    ♻️ 拆分 api/model：迁移用户相关模型

--- nonebot/adapters/discord/api/model.py
+++ nonebot/adapters/discord/api/model.py
@@ -96,6 +96,12 @@ from .models.polls import (
 )
 from .models.snowflake import Snowflake, SnowflakeType
 from .models.teams import Team, TeamMember, TeamMemberUser
+from .models.user import (
+    ApplicationRoleConnection,
+    AvatarDecorationData,
+    Connection,
+    User,
+)
 from .models.voice import VoiceRegion, VoiceState
 from .models.webhooks import ExecuteWebhookParams, Webhook
 from .types import (
@@ -110,7 +116,6 @@ from .types import (
     AttachmentFlag,
     ChannelFlags,
     ChannelType,
-    ConnectionServiceType,
     DefaultMessageNotificationLevel,
     EmbedTypes,
     EntitlementType,
@@ -156,7 +161,6 @@ from .types import (
     UserFlags,
     VerificationLevel,
     VideoQualityMode,
-    VisibilityType,
 )
 
 # Channel
@@ -1778,70 +1782,6 @@ class StickerPacksResponse(BaseModel):
     """
 
     sticker_packs: list[StickerPack]
-
-
-# User
-# see https://discord.com/developers/docs/resources/user
-class User(BaseModel):
-    """User
-
-    see https://discord.com/developers/docs/resources/user#user-object"""
-
-    id: Snowflake
-    username: str
-    discriminator: str
-    global_name: str | None = None
-    avatar: str | None = Field(...)
-    bot: Missing[bool] = UNSET
-    system: Missing[bool] = UNSET
-    mfa_enabled: Missing[bool] = UNSET
-    banner: MissingOrNullable[str] = UNSET
-    accent_color: MissingOrNullable[int] = UNSET
-    locale: Missing[str] = UNSET
-    verified: Missing[bool] = UNSET
-    email: MissingOrNullable[str] = UNSET
-    flags: Missing[int] = UNSET
-    premium_type: Missing[PremiumType] = UNSET
-    public_flags: Missing[UserFlags] = UNSET
-    avatar_decoration_data: MissingOrNullable["AvatarDecorationData"] = UNSET
-
-
-class AvatarDecorationData(BaseModel):
-    """Avatar Decoration Data
-
-    see https://discord.com/developers/docs/resources/user#avatar-decoration-data-object
-    """
-
-    asset: str
-    sku_id: Snowflake
-
-
-class Connection(BaseModel):
-    """Connection
-
-    see https://discord.com/developers/docs/resources/user#connection-object"""
-
-    id: str
-    name: str
-    type: ConnectionServiceType
-    revoked: Missing[bool] = UNSET
-    integrations: Missing[list["Integration"]] = UNSET  # partial server integrations
-    verified: bool
-    friend_sync: bool
-    show_activity: bool
-    two_way_link: bool
-    visibility: VisibilityType
-
-
-class ApplicationRoleConnection(BaseModel):
-    """Application Role Connection
-
-    see https://discord.com/developers/docs/resources/user#application-role-connection-object
-    """
-
-    platform_name: str | None = Field(...)
-    platform_username: str | None = Field(...)
-    metadata: dict  # object
 
 
 # Webhook
