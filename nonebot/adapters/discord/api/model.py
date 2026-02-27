c9221f024f3be5352c4f2f3ba897591419d6b405
shoucandanghehe <wallfjjd@gmail.com>
2026-02-28 03:22:51 +0800

    ♻️ 拆分 api/model：迁移 Soundboard 相关模型

--- nonebot/adapters/discord/api/model.py
+++ nonebot/adapters/discord/api/model.py
@@ -166,6 +166,14 @@ from .models.polls import (
     PollResults,
 )
 from .models.snowflake import Snowflake, SnowflakeType
+from .models.soundboard import (
+    CreateGuildSoundboardSoundParams,
+    ListDefaultSoundboardSoundsResponse,
+    ListGuildSoundboardSoundsResponse,
+    ModifyGuildSoundboardSoundParams,
+    SendSoundboardSoundParams,
+    SoundboardSound,
+)
 from .models.stage_instance import StageInstance
 from .models.stickers import Sticker, StickerItem, StickerPack, StickerPacksResponse
 from .models.teams import Team, TeamMember, TeamMemberUser
@@ -2098,75 +2106,6 @@ class BulkBan(BaseModel):
     """list of user ids, that were successfully banned"""
     failed_users: list[Snowflake]
     """list of user ids, that were not banned"""
-
-
-class SoundboardSound(BaseModel):
-    """Soundboard Sound Object
-
-    see https://discord.com/developers/docs/resources/soundboard#soundboard-sound-object
-    """
-
-    name: str
-    sound_id: Snowflake
-    volume: float
-    emoji_id: Snowflake | None = Field(...)
-    emoji_name: str | None = Field(...)
-    guild_id: Missing[Snowflake] = UNSET
-    available: bool
-    user: Missing["User"] = UNSET
-
-
-class SendSoundboardSoundParams(BaseModel):
-    """Send Soundboard Sound Params.
-
-    see https://discord.com/developers/docs/resources/soundboard#send-soundboard-sound
-    """
-
-    sound_id: Snowflake
-    source_guild_id: Missing[Snowflake] = UNSET
-
-
-class CreateGuildSoundboardSoundParams(BaseModel):
-    """Create Guild Soundboard Sound Params.
-
-    see https://discord.com/developers/docs/resources/soundboard#create-guild-soundboard-sound
-    """
-
-    name: str
-    sound: str
-    volume: Missing[float] = UNSET
-    emoji_id: Missing[Snowflake] = UNSET
-    emoji_name: Missing[str] = UNSET
-
-
-class ModifyGuildSoundboardSoundParams(BaseModel):
-    """Modify Guild Soundboard Sound Params.
-
-    see https://discord.com/developers/docs/resources/soundboard#modify-guild-soundboard-sound
-    """
-
-    name: Missing[str] = UNSET
-    volume: Missing[float] = UNSET
-    emoji_id: MissingOrNullable[Snowflake] = UNSET
-    emoji_name: MissingOrNullable[str] = UNSET
-
-
-class _SoundboardSoundsListResponse(BaseModel):
-    items: list[SoundboardSound]
-
-
-class ListGuildSoundboardSoundsResponse(_SoundboardSoundsListResponse):
-    """List Guild Soundboard Sounds Response.
-
-    see https://discord.com/developers/docs/resources/soundboard#list-guild-soundboard-sounds
-    """
-
-
-class ListDefaultSoundboardSoundsResponse(_SoundboardSoundsListResponse):
-    """List Default Soundboard Sounds Response.
-
-    see https://discord.com/developers/docs/resources/soundboard#list-default-soundboard-sounds
-    """
 
 
 # Lobby
---
+++ nonebot/adapters/discord/api/models/soundboard.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from pydantic import BaseModel, Field
+
+from .snowflake import Snowflake
+from ..types import UNSET, Missing, MissingOrNullable
+
+if TYPE_CHECKING:
+    from ..model import User
+
+
+class SoundboardSound(BaseModel):
+    """Soundboard Sound Object
+
+    see https://discord.com/developers/docs/resources/soundboard#soundboard-sound-object
+    """
+
+    name: str
+    sound_id: Snowflake
+    volume: float
+    emoji_id: Snowflake | None = Field(...)
+    emoji_name: str | None = Field(...)
+    guild_id: Missing[Snowflake] = UNSET
+    available: bool
+    user: Missing[User] = UNSET
+
+
+class SendSoundboardSoundParams(BaseModel):
+    """Send Soundboard Sound Params.
+
+    see https://discord.com/developers/docs/resources/soundboard#send-soundboard-sound
+    """
+
+    sound_id: Snowflake
+    source_guild_id: Missing[Snowflake] = UNSET
+
+
+class CreateGuildSoundboardSoundParams(BaseModel):
+    """Create Guild Soundboard Sound Params.
+
+    see https://discord.com/developers/docs/resources/soundboard#create-guild-soundboard-sound
+    """
+
+    name: str
+    sound: str
+    volume: Missing[float] = UNSET
+    emoji_id: Missing[Snowflake] = UNSET
+    emoji_name: Missing[str] = UNSET
+
+
+class ModifyGuildSoundboardSoundParams(BaseModel):
+    """Modify Guild Soundboard Sound Params.
+
+    see https://discord.com/developers/docs/resources/soundboard#modify-guild-soundboard-sound
+    """
+
+    name: Missing[str] = UNSET
+    volume: Missing[float] = UNSET
+    emoji_id: MissingOrNullable[Snowflake] = UNSET
+    emoji_name: MissingOrNullable[str] = UNSET
+
+
+class _SoundboardSoundsListResponse(BaseModel):
+    items: list[SoundboardSound]
+
+
+class ListGuildSoundboardSoundsResponse(_SoundboardSoundsListResponse):
+    """List Guild Soundboard Sounds Response.
+
+    see https://discord.com/developers/docs/resources/soundboard#list-guild-soundboard-sounds
+    """
+
+
+class ListDefaultSoundboardSoundsResponse(_SoundboardSoundsListResponse):
+    """List Default Soundboard Sounds Response.
+
+    see https://discord.com/developers/docs/resources/soundboard#list-default-soundboard-sounds
+    """
